b1b114952fff8affba6768d2acb2913f458ff76f
TravHaran <ratnahat@mcmaster.ca>
2021-01-01 23:22:32 -0500

    fixed smooth scroll for cross compatibility

--- js/typewriter.js
+++ js/typewriter.js
@@ -39,3 +39,46 @@ function erase(){
 document.addEventListener("DOMContentLoaded", function(){
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
+
+// Smooth Scrolling
+
+function scrollToSection(event,n) {
+  if (supportsSmoothScrolling()) {
+    return;
+  }
+  event.preventDefault();
+  if (n=0) section= "section-hero";
+  else if (n=1) section= "section-intro";
+  else if (n=2) section= "section-projects";
+  else if (n=3) section= "section-education";
+  else if (n=4) section= "section-skills";
+  const scrollToElem = document.getElementById(section);
+  SmoothVerticalScrolling(scrollToElem, 300, "top");
+
+}
+
+function supportsSmoothScrolling() {
+  const body = document.body;
+  const scrollSave = body.style.scrollBehavior;
+  body.style.scrollBehavior = 'smooth';
+  const hasSmooth = getComputedStyle(body).scrollBehavior === 'smooth';
+  body.style.scrollBehavior = scrollSave;
+  return hasSmooth;
+};
+
+function SmoothVerticalScrolling(element, time, position) {
+  var eTop = element.getBoundingClientRect().top;
+  var eAmt = eTop / 100;
+  var curTime = 0;
+  while (curTime <= time) {
+    window.setTimeout(SVS_B, curTime, eAmt, position);
+    curTime += time / 100;
+  }
+}
+
+function SVS_B(eAmt, position) {
+  if (position == "center" || position == "")
+  window.scrollBy(0, eAmt / 2);
+  if (position == "top")
+  window.scrollBy(0, eAmt);
+}
